2e04fa31ea4d834ec0c3b97e103c26aebd9833c0
Thekiborg <doflr415@gmail.com>
2025-11-04 22:12:44 +0100

    Push

--- Escenas/personaje.gd
+++ Escenas/personaje.gd
@@ -1,12 +1,19 @@
 extends CharacterBody2D
 
+class_name Character
+
+var healthPoints: int = 10;
 var movement_speed: float = 60.0
 var movement_target_position: Vector2 = Vector2(500.0,300.0)
-var target: Node2D;
+var target: CharacterBody2D;
+
+@onready var health_bar: ProgressBar = $HealthBar
 
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 
 func _ready():
+	health_bar.value = healthPoints
+	
 	# These values need to be adjusted for the actor's speed
 	# and the navigation layout.
 	navigation_agent.path_desired_distance = 4.0
@@ -41,7 +48,10 @@ func set_movement_target(movement_target: Vector2):
 	navigation_agent.target_position = movement_target
 
 func _physics_process(delta):
-	if target != null && movement_target_position != position:
+	if movement_target_position == position:
+		return
+	
+	if target != null:
 		movement_target_position = target.global_position
 		set_movement_target(movement_target_position)
 
@@ -51,8 +61,23 @@ func _physics_process(delta):
 		velocity = current_agent_position.direction_to(next_path_position) * movement_speed
 		var collided = move_and_slide()
 		if collided:
-			movement_target_position = position;
-			set_movement_target(movement_target_position)
+			var collider = get_last_slide_collision().get_collider();
+			if collider is Character:
+				collider.receive_damage(1)
+			#movement_target_position = position;
+			#set_movement_target(movement_target_position)
+	elif target == null:
+		movement_target_position = position
+		set_movement_target(movement_target_position)
+
+func receive_damage(damage: int):
+	healthPoints -= damage;
+	health_bar.value = healthPoints
+	if healthPoints <= 0:
+		die()
+
+func die():
+		queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
---
+++ Escenas/personaje_stronger.gd
@@ -0,0 +1,7 @@
+extends Character
+
+class_name CharacterStronger 
+
+func _ready():
+	healthPoints = 100
+	super._ready()
